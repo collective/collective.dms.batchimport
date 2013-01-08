4ef447a9791260839b05a32af06dd7c32a309c6e
David Convent <davconvent@bruxxel.org>
2013-01-08 17:22:52 +0100

    first import of package files

---
+++ src/collective/dms/batchimport/batchimport.py
@@ -0,0 +1,32 @@
+from zope.interface import Interface
+from zope import schema
+
+from Products.Five.browser import BrowserView
+
+from plone.autoform.directives import widget
+from collective.Z3cform.datagridfield import DataGridFieldFactory, DictRow
+
+
+from . import _
+
+class ICodeTypeMapSchema(Interface):
+    code = schema.TextLine(title=_("Code"))
+    portal_type = schema.TextLine(title=_("Portal Type"))
+
+class ISettings(Interface):
+    fs_root_directory = schema.TextLine(
+        title=_("FS Root Directory"))
+
+    code_to_type_mapping = schema.List(
+        title=_("Code to Portal Type Mapping"),
+        value_type=DictRow(title=_("Mapping"),
+                           schema=ICodeTypeMapSchema)
+        )
+    widget(code_to_type_mapping=DataGridFieldFactory)
+
+class BatchImporter(BrowserView):
+    pass
+
+class ControlPanel(BrowserView):
+    pass
+
